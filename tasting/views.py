ccd75ae1e4e63d8fc85424ab4f4671d00d78f2c2
Chris Beliveau <cdb2863@rit.edu>
2018-08-31 00:34:07 -0400

    make a useful index view and template

--- tasting/views.py
+++ tasting/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Wine
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test index for wine tasting app.")
+    wines = Wine.objects.order_by('Name')
+    context = {'wines': wines}
+    return render(request, 'tasting/index.html', context)
