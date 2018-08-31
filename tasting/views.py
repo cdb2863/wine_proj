d37ee3206580238c6f889ba90f2352581a60df04
Chris Beliveau <cdb2863@rit.edu>
2018-08-31 00:41:56 -0400

    implements detail view and template

--- tasting/views.py
+++ tasting/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Wine
 
 
@@ -8,3 +8,10 @@ def index(request):
     wines = Wine.objects.order_by('Name')
     context = {'wines': wines}
     return render(request, 'tasting/index.html', context)
+
+def wine_detail(request, wine_id):
+    try:
+        wine = Wine.objects.get(pk=wine_id)
+    except Wine.DoesNotExist:
+        raise Http404("Wine not found!")
+    return render(request, 'tasting/detail.html', {'wine': wine})
