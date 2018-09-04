75bf179c6c1269125cea579c52fd619dc15cfd8b
Chris Beliveau <cdb2863@rit.edu>
2018-09-04 15:30:11 -0400

    redirects / to /wine

--- wine_proj/urls.py
+++ wine_proj/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
+    re_path(r'^', include('tasting.urls')),
     path('wine/', include('tasting.urls')),
     path('admin/', admin.site.urls),
 ]
